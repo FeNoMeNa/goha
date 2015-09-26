0b2228ab94e396919de21cdd86775194aa9101cb
Ivan Stefanov <ivan.stefanov@clouway.com>
2015-09-26 10:23:01 +0300

    add timeout support

--- goha.go
+++ goha.go
@@ -1,15 +1,53 @@
 package goha
 
 import (
+	"io"
 	"net/http"
+	"net/url"
 	"strings"
+	"time"
 )
 
-// NewClient creates and initializes a new http client that will be able to
-// authorize its requests via Basic / Digest authentication scheme.
-func NewClient(username, password string) *http.Client {
+// Client is a wrapper to http.client. It is used as builder to construct
+// the real http client
+type Client struct {
+	client *http.Client
+}
+
+// NewClient creates and initializes a new goha http client that will be able
+// to authorize its requests via Basic / Digest authentication scheme.
+func NewClient(username, password string) *Client {
 	t := &transport{username, password}
-	return &http.Client{Transport: t}
+	c := &http.Client{Transport: t}
+
+	return &Client{client: c}
+}
+
+// Timeout initializes the default timeout of the http client. A Timeout of zero
+// means no timeout.
+func (c *Client) Timeout(t time.Duration) *Client {
+	c.client.Timeout = t
+	return c
+}
+
+func (c *Client) Do(req *http.Request) (resp *http.Response, err error) {
+	return c.client.Do(req)
+}
+
+func (c *Client) Get(url string) (resp *http.Response, err error) {
+	return c.client.Get(url)
+}
+
+func (c *Client) Post(url string, bodyType string, body io.Reader) (resp *http.Response, err error) {
+	return c.client.Post(url, bodyType, body)
+}
+
+func (c *Client) PostForm(url string, data url.Values) (resp *http.Response, err error) {
+	return c.client.PostForm(url, data)
+}
+
+func (c *Client) Head(url string) (resp *http.Response, err error) {
+	return c.client.Head(url)
 }
 
 // transport is an implementation of http.RoundTripper that takes care of
@@ -47,6 +85,16 @@ func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	}
 
 	return http.DefaultTransport.RoundTrip(creq)
+}
+
+func (t *transport) CancelRequest(req *http.Request) {
+	type canceler interface {
+		CancelRequest(*http.Request)
+	}
+
+	tr, _ := http.DefaultTransport.(canceler)
+
+	tr.CancelRequest(req)
 }
 
 // cloneRequest returns a clone of the provided *http.Request.
