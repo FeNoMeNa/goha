c1beaf39ce047a390c5fa643bab2bbab645cd8b4
MichaelS11 <MichaelS11@users.noreply.github.com>
2018-09-10 13:51:26 -0700

    Added NewClientWithTransport

--- goha.go
+++ goha.go
@@ -18,7 +18,15 @@ type Client struct {
 // NewClient creates and initializes a new goha http client that will be able
 // to authorize its requests via Basic / Digest authentication scheme.
 func NewClient(username, password string) *Client {
-	t := &transport{username, password}
+	t := &transportStruct{username: username, password: password, transport: http.DefaultTransport}
+	c := &http.Client{Transport: t}
+
+	return &Client{client: c}
+}
+
+// NewClient creates a new goha http client with provided http.Transport
+func NewClientWithTransport(username, password string, transport http.RoundTripper) *Client {
+	t := &transportStruct{username: username, password: password, transport: transport}
 	c := &http.Client{Transport: t}
 
 	return &Client{client: c}
@@ -31,6 +39,7 @@ func (c *Client) Timeout(t time.Duration) *Client {
 	return c
 }
 
+// Jar sets Client.Jar to passed cookiejar.Jar
 func (c *Client) Jar(j *cookiejar.Jar) *Client {
 	c.client.Jar = j
 	return c
@@ -56,19 +65,20 @@ func (c *Client) Head(url string) (resp *http.Response, err error) {
 	return c.client.Head(url)
 }
 
-// transport is an implementation of http.RoundTripper that takes care of
+// transportStruct is an implementation of http.RoundTripper that takes care of
 // http authentication.
-type transport struct {
-	username string
-	password string
+type transportStruct struct {
+	username  string
+	password  string
+	transport http.RoundTripper
 }
 
 // RoundTrip makes an authorized requests. First it sends a http request to
 // obtain the authentication challenge and then authorizes the request via
 // Basic / Digest authentication scheme.
-func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
+func (t *transportStruct) RoundTrip(req *http.Request) (*http.Response, error) {
 	// Make a request to get the 401 that contains the challenge.
-	resp, err := http.DefaultTransport.RoundTrip(req)
+	resp, err := t.transport.RoundTrip(req)
 
 	if err != nil || resp.StatusCode != 401 {
 		return resp, err
@@ -90,17 +100,17 @@ func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
 		return resp, err
 	}
 
-	return http.DefaultTransport.RoundTrip(creq)
+	return t.transport.RoundTrip(creq)
 }
 
 // CancelRequest cancels an in-flight request by closing its connection.
 // CancelRequest should only be called after RoundTrip has returned.
-func (t *transport) CancelRequest(req *http.Request) {
+func (t *transportStruct) CancelRequest(req *http.Request) {
 	type canceler interface {
 		CancelRequest(*http.Request)
 	}
 
-	tr, _ := http.DefaultTransport.(canceler)
+	tr, _ := t.transport.(canceler)
 
 	tr.CancelRequest(req)
 }
